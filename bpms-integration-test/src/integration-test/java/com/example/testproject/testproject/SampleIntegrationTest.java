eea98f54cbc4041a4c67d5af46e8c35629a7a7c5
Paul Brown <pauljamesbrown>
2018-03-02 16:30:09 +0000

    Initial Commit

---
+++ bpms-integration-test/src/integration-test/java/com/example/testproject/testproject/SampleIntegrationTest.java
@@ -0,0 +1,15 @@
+package com.example.testproject.testproject;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class SampleIntegrationTest{
+	
+	@Test
+	public void dummyIntegrationTest(){
+		assertTrue(true);
+		System.out.print("Sample Integration Test Completed");
+		
+	}
+	
+}
---
+++ bpms-integration-test/src/test/java/com/example/testproject/testproject/SampleUnitTest.java
@@ -0,0 +1,18 @@
+package com.example.testproject.testproject;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class SampleUnitTest {
+
+		@Test
+		public void BasicSampleUnitTest(){
+			assertTrue(true);
+			System.out.print("Sample Unit Test Completed");
+			
+		}
+		
+}
+	
+	
+
